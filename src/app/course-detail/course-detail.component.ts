0ab41e44ef735bfcd7b5673384b3f19ccc3c675a
Raphael Caputo <raphael.caputo@agenciamacro.com.br>
2020-04-09 15:25:43 -0300

    Nested Subs and Buble-Event solved

--- src/app/course-detail/course-detail.component.ts
+++ src/app/course-detail/course-detail.component.ts
@@ -6,6 +6,8 @@ import * as _ from 'lodash';
 import {CoursesService} from "../services/courses.service";
 import {NewsletterService} from "../services/newsletter.service";
 import {UserService} from "../services/user.service";
+import { Observable } from 'rxjs';
+import { switchMap, first, publishLast, refCount } from 'rxjs/operators';
 
 
 @Component({
@@ -15,8 +17,8 @@ import {UserService} from "../services/user.service";
 })
 export class CourseDetailComponent implements OnInit {
 
-  course: Course;
-  lessons: Lesson[];
+  course$: Observable<Course>;
+  lessons$: Observable<Lesson[]>;
 
   constructor(private route: ActivatedRoute,
               private coursesService: CoursesService,
@@ -25,31 +27,66 @@ export class CourseDetailComponent implements OnInit {
 
   }
 
-  onSubscribe(email:string) {
-      this.newsletterService.subscribeToNewsletter(email)
-          .subscribe(
-              () => {
-                  alert('Subscription successful ...');
-              },
-              console.error
-          );
+  ngOnInit() {
+    
+    this.course$ = this.route.params.pipe(
+      switchMap(params => this.coursesService.findCourseByUrl(params['id'])),
+      
+      // Ensure completion on the first emitted value
+      first(),
+
+      // it won’t emit any value until Source completes.
+      // When Source completes AsyncSubject completes too and emits last value
+      // and ‘complete’ notification to all subscribers, current and new.
+      publishLast(),
+
+      // Connecting and disconnecting manually might be hard to implement,
+      // therefore there is a refCount() operator that will automatically connect()
+      // with first subscription, keep count of subscriptions and keep Subject connected
+      // to the Source as long as there is at least one subscriber.
+      refCount()
+    );
+
+    // Since we need the course to be able to load it's lessons
+    // We are going to derive the lessons Observable from the course Observable
+    // getting the course that is been emitted from course$ and load the lessons
+    this.lessons$ = this.course$.pipe(
+      switchMap(course => this.coursesService.findLessonsForCourse(course.id)),
+      first(),
+      publishLast(),
+      refCount()
+    )
+
+    // Solved the nested subscribe problem is above
+    // Nested subscribe problem
+      // this.route.params
+      //     .subscribe( params => {
+
+      //         const courseUrl = params['id'];
+
+      //         this.coursesService.findCourseByUrl(courseUrl)
+      //             .subscribe(data => {
+      //                 this.course = data;
+
+      //                 this.coursesService.findLessonsForCourse(this.course.id)
+      //                     .subscribe(lessons => this.lessons = lessons);
+      //             });
+
+      //     });
   }
 
-  ngOnInit() {
-      this.route.params
-          .subscribe( params => {
+  onSubscribe(email:string) {
+    this.newsletterService.subscribeToNewsletter(email)
+        .subscribe(
+            () => {
+                alert('Subscription successful ...');
+            },
+            console.error
+        );
+  }
 
-              const courseUrl = params['id'];
-
-              this.coursesService.findCourseByUrl(courseUrl)
-                  .subscribe(data => {
-                      this.course = data;
-
-                      this.coursesService.findLessonsForCourse(this.course.id)
-                          .subscribe(lessons => this.lessons = lessons);
-                  });
-
-          });
+  loginAsJohn() {
+    this.userService.login('john@gmail.com', 'test123').subscribe()
   }
 
 }
--- src/app/newsletter/newsletter.component.ts
+++ src/app/newsletter/newsletter.component.ts
@@ -1,22 +1,38 @@
 import {Component, OnInit, Input, Output, EventEmitter} from '@angular/core';
+import { UserService } from 'app/services/user.service';
+import { NewsletterService } from 'app/services/newsletter.service';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Component({
     selector: 'newsletter',
     templateUrl: './newsletter.component.html',
     styleUrls: ['./newsletter.component.css']
 })
-export class NewsletterComponent {
+export class NewsletterComponent implements OnInit {
 
-    @Input()
-    firstName:string;
+    firstName$: Observable<string>;
 
-    @Output()
-    subscribe = new EventEmitter();
+    constructor(
+        private userService: UserService,
+        private newsletterService: NewsletterService) {}
 
-    subscribeToNewsletter(emailField) {
-        this.subscribe.emit(emailField.value);
-        emailField.value = '';
+    ngOnInit() {
+        this.firstName$ = this.userService.user$.pipe(
+            map(user => user.firstName)
+        )
     }
 
+
+    subscribeToNewsletter(emailField) {
+        this.newsletterService.subscribeToNewsletter(emailField.value)
+            .subscribe(
+                () => {
+                    emailField.value = '';
+                    alert('Subscription successful ...')
+                },
+                console.error
+            );
+    }
 
 }
